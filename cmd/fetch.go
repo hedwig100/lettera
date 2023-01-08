8b8a6fd94d3b7d640daba9df1c5bee6ef5e7168b
hedwig100 <pentagon7951@outlook.jp>
2023-01-08 17:14:43 +0900

    [add] fetchしたときにmarkdownに書き込む機能を追加

--- cmd/fetch.go
+++ cmd/fetch.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 
 	"github.com/hedwig100/lettera/bib"
+	"github.com/hedwig100/lettera/paper"
 	"github.com/spf13/cobra"
 )
 
 var (
-	doi         string
-	bibfilePath string
+	doi          string
+	bibfilePath  string
+	markdownPath string
+	note         string
 
 	fetchCmd = &cobra.Command{
 		Use:   "fetch",
@@ -24,6 +27,11 @@ var (
 			if err != nil {
 				return err
 			}
+			p := paper.NewPaper(b, doi, note)
+			err = paper.WriteToMarkdown(p, markdownPath)
+			if err != nil {
+				return err
+			}
 			fmt.Println(b)
 			return nil
 		},
@@ -34,5 +42,7 @@ func init() {
 	rootCmd.AddCommand(fetchCmd)
 	fetchCmd.Flags().StringVarP(&doi, "doi", "d", "", "DOI of the paper you want to fetch")
 	fetchCmd.Flags().StringVarP(&bibfilePath, "bibfile", "b", "lettera.bib", "filepath that you want to write the fetched bib data to")
+	fetchCmd.Flags().StringVarP(&markdownPath, "markdown", "m", "lettera.md", "markdown filepath that you want to write the fetched paperdata to")
+	fetchCmd.Flags().StringVarP(&note, "note", "n", "", "what you want to note about the reference paper")
 	fetchCmd.MarkFlagRequired("doi")
 }
